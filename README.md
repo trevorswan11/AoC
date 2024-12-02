aa94d2abf7421a381b380e712c5d3c2afcef1d4d
Trevor Swan <tcs94@case.edu>
2024-12-02 12:47:54 -0500

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code
+Various AoC puzzles and their code solutions. Puzzles are named according to their date, and are all made as text files.
---
+++ src/main/java/2024/solutions/App.java
@@ -0,0 +1,13 @@
+package 2024.solutions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
