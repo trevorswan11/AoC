1f1bc09a8e0c57fc68bc2094a63a6bf6ea9037bc
trevorswan11 <tcs94@case.edu>
2025-12-08 18:51:46 -0500

    I dont know whats going on i need to reference a solution.

--- aoc2025/day7/index.ts
+++ aoc2025/day7/index.ts
@@ -1,5 +1,5 @@
-// const input_path: string = "input_2025-day7.txt";
-const input_path: string = "input_test.txt";
+const input_path: string = "input_2025-day7.txt";
+// const input_path: string = "input_test.txt";
 
 async function readInputLines(path: string): Promise<Array<string>> {
     const input = await Bun.file(path).text();
@@ -39,7 +39,38 @@ async function one(): Promise<number> {
 
 async function two(): Promise<number> {
     const lines = await readInputLines(input_path);
-    return lines.length;
+    let beams = new Array<number>(lines[0]!.length).fill(0);
+
+    // The first line starts the beam with an S, and there is only one
+    const first_line = lines[0]!;
+    beams[first_line.indexOf("S")] = 1;
+
+    // The lines in between splitters aren't necessary, and neither is the last line
+    for (let i = 2; i < lines.length; i += 2) {
+        const line = lines[i]!;
+
+        for (let j = 0; j < line.length; j++) {
+            // We only care about where a splitter is, which is given by a carrot
+            if (line[j]! === "^" && beams[j]! != 0) {
+                // Hitting a splitter blocks below but propagates left and right at the same time
+                if (beams[j - 1]! == 0) {
+                    beams[j - 1]! = beams[j]!;
+                } else {
+                    beams[j - 1]! += beams[j]!;
+                }
+
+                if (beams[j + 1]! == 0) {
+                    beams[j + 1]! = beams[j]!;
+                } else {
+                    beams[j + 1]! += beams[j]!;
+                }
+
+                beams[j]! = 0;
+            }
+        }
+    }
+
+    return beams.reduce((a, b) => a + b);
 }
 
 const part_one = await one();
