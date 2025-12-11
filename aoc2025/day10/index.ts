37faf97d57b5da924c3cbfebe8c712700f7362d8
trevorswan11 <tcs94@case.edu>
2025-12-10 23:04:00 -0500

    I might ditch this and just do part two in C# becuase im really lost!

--- aoc2025/day10/index.ts
+++ aoc2025/day10/index.ts
@@ -1,3 +1,6 @@
+import { init } from 'z3-solver'
+const { Context } = await init();
+
 // const input_path: string = "input_2025-day10.txt";
 const input_path: string = "input_test.txt";
 
@@ -9,18 +12,23 @@ async function readInputLines(path: string): Promise<Array<string>> {
 // The input gives these as little endian, but machines are parsed as big endian
 class Machine {
     // A bitset where bit at i is if it needs to be toggled.
-    readonly indicator: number;
+    readonly indicator_bits: number;
 
     // A bitset where bit at i means button j (in array) toggles all corresponding bits
-    readonly wirings: Array<number>;
+    readonly wirings_bits: Array<number>;
+    readonly wirings: Array<Array<number>>;
 
     // Numbers that correspond to the target joltage for each bit in the indicator
     // Pressing a button means that its bits increment their joltage by 1
     readonly joltages: Array<number>;
 
-    constructor(desired_indicator: number, wiring_schematics: Array<number>, joltage_requirements: Array<number>) {
-        this.indicator = desired_indicator;
-        this.wirings = wiring_schematics;
+    constructor(desired_indicator: number,
+        wiring_schematic_bits: Array<number>,
+        wiring_schematic_nums: Array<Array<number>>,
+        joltage_requirements: Array<number>) {
+        this.indicator_bits = desired_indicator;
+        this.wirings_bits = wiring_schematic_bits;
+        this.wirings = wiring_schematic_nums;
         this.joltages = joltage_requirements;
     }
 }
@@ -45,24 +53,28 @@ async function parseInputLines(path: string): Promise<Array<Machine>> {
         }
 
         // We can safely decompose wirings into binary values due to input constraints
-        let wirings = new Array<number>(wir_strs.length);
-        for (let w = 0; w < wirings.length; w++) {
+        let wiring_bits = new Array<number>(wir_strs.length);
+        let wiring_nums = new Array<Array<number>>(wir_strs.length);
+        for (let w = 0; w < wiring_bits.length; w++) {
             const wiring_list = wir_strs[w]!;
+            wiring_nums[w] = wiring_list
+                .slice(1, wiring_list.length - 1)
+                .split(',')
+                .map(value => +value);
 
             // Each button layout in surrounded by parentheses
             let wiring = 0;
-            for (let entangled of wiring_list.slice(1, wiring_list.length - 1).split(',')) {
-                const shift = +entangled;
+            for (let shift of wiring_nums[w]!) {
                 wiring |= (1 << shift);
             }
-            wirings[w] = wiring;
+            wiring_bits[w] = wiring;
         }
 
         // Joltages are just decimal numbers and have been stripped of delimiters
         let joltages = new Array<number>(jol_strs.length);
         jol_strs.forEach((value, index) => joltages[index] = parseInt(value));
 
-        machines[i] = new Machine(indicator, wirings, joltages);
+        machines[i] = new Machine(indicator, wiring_bits, wiring_nums, joltages);
     }
 
     return machines;
@@ -89,12 +101,12 @@ function one(machines: Array<Machine>): number {
         let best_subset = Number.MAX_SAFE_INTEGER;
 
         // Super brute force, but reduction is xor so order independent
-        for (const set of getButtonPowerset(machine.wirings)) {
+        for (const set of getButtonPowerset(machine.wirings_bits)) {
             // The power set contains the empty set by definition
             if (set.length == 0) continue;
 
             const result = set.reduce((a, b) => a ^ b);
-            if (machine.indicator == result) {
+            if (machine.indicator_bits == result) {
                 best_subset = Math.min(best_subset, set.length);
             }
         }
@@ -105,27 +117,59 @@ function one(machines: Array<Machine>): number {
     return sum_of_fewest;
 }
 
-// Brainstorming for part 2
-// Example: [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-/*
+async function optimizeMachine(machine: Machine): Promise<number> {
+    const Z3 = Context('main');
 
+    // Define all constants (button wirings)
+    let variables = machine.wirings.map(button => Z3.Int.const(button.join(",")));
+    let opt = new Z3.Optimize();
 
+    // Propagate the optimizer with all of the joltage constraints
+    for (let i = 0; i < machine.joltages.length; i++) {
+        let vars: typeof variables = [];
+        for (let j = 0; j < machine.wirings.length; j++) {
+            if (machine.wirings[j]!.includes(i)) {
+                vars.push(variables[j]!);
+            }
+        }
+        console.log("Z3 initialized");
 
-*/
+        // Sum all of the relevant variables
+        const sum = vars.length === 1
+            ? vars[0]!
+            : Z3.Sum(vars[0]!, ...vars.slice(1));
+        let constraint = Z3.Eq(sum, Z3.Int.val(machine.joltages[i]!));
+        opt.add(constraint);
+    }
 
-async function twoZ3(machines: Array<Machine>): Promise<number> {
-    let sum_of_fewest = 0;
+    // Now we can attempt to minimize
+    const total = variables.length === 1
+        ? variables[0]!
+        : Z3.Sum(variables[0]!, ...variables.slice(1));
+    opt.minimize(total);
+    for (let v of variables) {
+        opt.add(Z3.GE(v, Z3.Int.val(0)));
+    }
 
-    return sum_of_fewest;
+    let result = await opt.check();
+    if (result !== 'sat' && result !== 'unknown') {
+        throw new Error("Unsatisfiable machine constraints");
+    }
+
+    let model = opt.model();
+    return variables.map(v => model.eval(v).ast.valueOf()).reduce((a, b) => a + b);
 }
 
-function two(machines: Array<Machine>): number {
+async function two(machines: Array<Machine>): Promise<number> {
     let sum_of_fewest = 0;
+    for (let machine of machines) {
+        sum_of_fewest += await optimizeMachine(machine);
+    }
     return sum_of_fewest;
 }
 
 const machines = await parseInputLines(input_path);
 const part_one = one(machines);
 console.log("Part one result: ", part_one);
-const part_two = two(machines);
+const part_two = await two(machines);
 console.log("Part two result: ", part_two);
